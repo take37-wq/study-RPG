d73a288fb650248a8e1e2e6995f11350b4af13bd
take37-wq <take030737@gmail.com>
2026-02-06 22:05:34 +0900

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME="study-pop-rpg-v1";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./images/Piskel-1.png",
+  "./images/Piskel-2.png",
+  "./images/Piskel-3.png"
+];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
